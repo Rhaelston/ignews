c48fdbc12ef32010f54ea3e5c97d3fa865c39489
Rhaelston <rhaelston@hotmail.com>
2022-03-15 20:34:10 -0300

    add the sign out

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,7 +1,7 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 
 export function SignInButton () {
@@ -12,7 +12,8 @@ export function SignInButton () {
     return Session ?( 
        <button
         type='button'
-        className={styles.signInButton}     
+        className={styles.signInButton}
+        onClick={()=>signOut()}  
        >
         <FaGithub color="#04d361" />
             {Session.user.name}
