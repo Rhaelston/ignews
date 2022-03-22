a90239a5f26a76d28ed1372de4a58f9bd96b45f5
Rhaelston <rhaelston@hotmail.com>
2022-03-22 05:31:12 -0300

    subscribe button

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {fauna} from '../../../services/fauna'
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -11,8 +14,41 @@ export default NextAuth({
         params: {
           scope: 'read:user'
         }
-      }
+      },
+
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user,}) {
+      const {email} = user
+
+      try {await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            {data: {email} }
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        )
+      )
+        return true
+      } catch {
+        return false
+      }
+    }, 
+  },
 })
